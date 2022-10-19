12c6acc9d43eb589144258693958f39765bc8bd3
lmejn <33491793+lmejn@users.noreply.github.com>
2022-10-20 10:19:39 +1100

    Modified water-phantom example
    - Added three examples of dose profiles
    - Added comments
    - Exported GantryPosition for use in the example

--- examples/water-phantom.jl
+++ examples/water-phantom.jl
@@ -1,17 +1,22 @@
 #=
-    Compute the dose on a water phantom for a square field
+    Compute the dose in a water phantom for a square field
 
-Compute the dose at various depths in a water phantom, with a 10x10cm² square
-field. Uses a plane surface, located at SAD.
+Compute the dose at various depths and off-axis positions in a water phantom
+with a 10x10cm² square field. Uses a plane surface, located at SAD.
+
+Computes the dose for three dose profiles:
+- The depth-dose curve along the central axis
+- The off-axis profile at a depth of 100 mm
+- A 2D dose distribution.
 =#
 
 using DoseCalculations
-using Plots, Meshes, LsqFit
+using Plots, StaticArrays
+
+#--- Setup Dose Calculation --------------------------------------------------------------------------------------------
 
 # Set Fieldsize
 fieldsize = 100.
-ϕg = 0.
-θb = 0.
 SAD = 1000.
 
 SSD = SAD
@@ -26,22 +31,57 @@ calc = ScaledIsoplaneKernel("examples/sample-data/dose-kernel/scaled-isoplane-ke
 MU = 100.
 calibrate!(calc, MU, fieldsize, SSD)
 
-# Create dose points in IEC BLD coordinates
-zp = -SAD-400:1.:-SAD
-pos = Vec.(0., 0., zp)
-
 # Create External Surface
 surf = PlaneSurface(SAD)
 
-depth = getdepth.(Ref(surf), pos)
+gantry = GantryPosition(0., 0., SAD)
 
-# Create dose-fluence matrix
-@time dose = dose_fluence_matrix(pos, vec(bixels), surf, calc)
+#--- Depth Dose --------------------------------------------------------------------------------------------------------
 
-depth = getdepth.(Ref(surf), pos)
+# Create dose points
+zp = -400:1.:0
+pos = SVector.(0., 0., zp)
+
+# Calculate dose
+@time depth_dose = dose_fluence_matrix(pos, vec(bixels), gantry, surf, calc)[1, :]
+
 begin
+    depth = getdepth.(Ref(surf), fixed_to_bld(gantry).(pos))
     p = plot(xlabel="Depth (mm)", ylabel="Dose (Gy)", ylim=[0, Inf])
-    plot!(depth, MU*vec(dose), label="Calculated")
-    plot!(depth, MU*norm_depth_dose.(Ref(calc), depth), label="Measured")
+    plot!(depth, MU*vec(depth_dose), label="Calculated")
+    plot!(depth, MU*DoseCalculations.norm_depth_dose.(Ref(calc), depth))
     hline!([1.], color=:black, label="")
+end
+
+#--- Off-Axis Profile --------------------------------------------------------------------------------------------------
+
+# Create dose points
+depth = 100.
+xp = -200.:1.:200.
+pos = SVector.(xp, 0., -depth)
+
+# Calculate dose
+@time offaxis_dose = dose_fluence_matrix(pos, vec(bixels), gantry, surf, calc)[1, :]
+
+# Plot
+begin
+    p = plot(xlabel="Off-Axis Position (mm)", ylabel="Dose (Gy)", ylim=[0, Inf])
+    plot!(xp, MU*offaxis_dose, label="Depth=$depth mm")
+end
+
+#--- 2D Dose -----------------------------------------------------------------------------------------------------------
+
+# Create dose points
+depth = 0:5.:400
+offaxis_pos = -200.:5.:200.
+pos = DoseGrid(offaxis_pos, [0.], -depth)
+
+# Calculate dose
+@time dose = MU*dose_fluence_matrix(pos, vec(bixels), gantry, surf, calc)
+dose = reshape(Array(dose), size(pos));
+
+# Plot
+begin
+    p = plot(xlabel="Off-Axis Position (mm)", ylabel="Depth (mm)")
+    heatmap!(p, offaxis_pos, depth, dose[:, 1, :]', colorbar_title="Dose (Gy)")
 end
