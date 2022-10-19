59b7d1ea60df048ae8329c4fcfb9361ff8a766f0
lmejn <33491793+lmejn@users.noreply.github.com>
2022-10-19 18:50:08 +1100

    Rearranged DosePoints.jl

--- src/DosePoints.jl
+++ src/DosePoints.jl
@@ -4,18 +4,122 @@ using SparseArrays, StaticArrays
 
 export save, DoseGrid, CylinderBounds, gridsize, MeshBounds
 
+#--- Dose Positions ----------------------------------------------------------------------------------------------------
 
-# Dose Positions
 abstract type DosePositions end
 
 Base.iterate(pos::DosePositions) = iterate(pos, 1)
 function Base.iterate(pos::DosePositions, i)
-    if(i>length(pos)) return nothing end
+    i>length(pos) && return nothing
     pos[i], i+1
 end
 
-function Base.:+(pos::T, x) where T<:DosePositions
-    T(pos.x .+ x[1], pos.y .+ x[2], pos.z .+ x[3])
+#--- DoseGrid ----------------------------------------------------------------------------------------------------------
+
+struct DoseGrid{Tx<:AbstractVector, Ty<:AbstractVector, Tz<:AbstractVector} <: DosePositions
+    x::Tx
+    y::Ty
+    z::Tz
+    indices::Vector{CartesianIndex{3}}
+    cells::Vector{Vector{Int}}
+end
+
+function DoseGrid(Δ, bounds::AbstractBounds)
+    xmin, xmax, ymin, ymax, zmin, zmax = boundsextent(bounds)
+
+    x = xmin:Δ:xmax
+    y = ymin:Δ:ymax
+    z = zmin:Δ:zmax
+
+    grid_index = CartesianIndices( (length(x), length(y), length(z)) )
+    indices = CartesianIndex{3}[]
+    linear_index = zeros(Int, length(x), length(y), length(z))
+
+    # Points
+    for i in grid_index
+        p = SVector(x[i[1]], y[i[2]], z[i[3]])
+        if(within(bounds, p))
+            push!(indices, grid_index[i])
+            linear_index[i] = length(indices)
+        end
+    end
+
+    # Cells
+
+    neighbours = vec([CartesianIndex(i,j,k) for i=0:1, j=0:1, k=0:1])[2:end]
+
+    cells = Vector{Int}[]
+    for i in eachindex(indices)
+        cell = [i]
+        for n in neighbours
+            index = indices[i] + n
+
+            if(checkbounds(Bool, linear_index, index) && linear_index[index] != 0)
+                push!(cell, linear_index[index])
+            end
+        end
+        if(length(cell)==8)
+            push!(cells, cell)
+        end
+    end
+
+    DoseGrid(x, y, z, indices, cells)
+end
+
+Base.:+(pos::DoseGrid, x) = DoseGrid(pos.x .+ x[1], pos.y .+ x[2], pos.z .+ x[3], pos.indices)
+
+Base.length(pos::DoseGrid) = length(pos.indices)
+Base.size(pos::DoseGrid) = (length(pos),)
+
+gridsize(pos::DoseGrid) = (length(pos.x), length(pos.y), length(pos.z))
+
+Base.getindex(pos::DoseGrid, i::Int, j::Int, k::Int) = SVector(pos.x[i], pos.y[j], pos.z[k])
+Base.getindex(pos::DoseGrid, i::CartesianIndex{3}) = pos[i[1], i[2], i[3]]
+Base.getindex(pos::DoseGrid, i::Int) = pos[pos.indices[i]]
+
+Base.eachindex(pos::DoseGrid) = eachindex(pos.indices)
+Base.CartesianIndices(pos::DoseGrid) = pos.indices
+
+#--- IO
+
+function Base.show(io::IO, pos::DoseGrid)
+    print(io, "x=")
+    Base.show(io, pos.x)
+    print(io, " y=")
+    Base.show(io, pos.y)
+    print(io, " z=")
+    Base.show(io, pos.z)
+    println(io, " npts=", length(pos.indices))
+end
+
+function vtk_create_cell(cell)
+    n = length(cell)
+    n==5 && return MeshCell(VTKCellTypes.VTK_PYRAMID, cell)
+    n==6 && return MeshCell(VTKCellTypes.VTK_WEDGE, cell)
+    n==8 && return MeshCell(VTKCellTypes.VTK_VOXEL, cell)
+    
+end
+
+function vtk_generate_file(filename, pos::DoseGrid)
+    points = [pos[i] for i in eachindex(pos)]
+    cells = [vtk_create_cell(cell) for cell in pos.cells]
+    vtk_grid(filename, points, cells)
+end
+
+function vtk_grid_data(pos::DoseGrid, arr)  
+    grid_arr = zeros(size(pos))
+    for i in eachindex(arr)
+        grid_arr[pos.indices[i]] = arr[i]
+    end
+    grid_arr
+end
+
+function save(filename::String, pos::DoseGrid, data)
+    vtkfile = vtk_generate_file(filename, pos)
+    for (key, value) in data
+        vtkfile[key, VTKPointData()] = value
+    end
+    vtk_save(vtkfile)
 end
 
 #--- Bounds ------------------------------------------------------------------------------------------------------------
@@ -132,115 +236,3 @@ function within(bounds::MeshBounds, p)
     length(pI) % 2 != 0
 end
 
-# DoseGrid
-
-struct DoseGrid{Tx<:AbstractVector, Ty<:AbstractVector, Tz<:AbstractVector}
-    x::Tx
-    y::Ty
-    z::Tz
-    indices::Vector{CartesianIndex{3}}
-    cells::Vector{Vector{Int}}
-end
-
-
-function Base.show(io::IO, pos::DoseGrid)
-    print(io, "x=")
-    Base.show(io, pos.x)
-    print(io, " y=")
-    Base.show(io, pos.y)
-    print(io, " z=")
-    Base.show(io, pos.z)
-    println(io, " npts=", length(pos.indices))
-end
-Base.:+(pos::DoseGrid, x) = DoseGrid(pos.x .+ x[1], pos.y .+ x[2], pos.z .+ x[3], pos.indices)
-
-Base.length(pos::DoseGrid) = length(pos.indices)
-Base.size(pos::DoseGrid) = (length(pos),)
-
-gridsize(pos::DoseGrid) = (length(pos.x), length(pos.y), length(pos.z))
-
-Base.getindex(pos::DoseGrid, i::Int, j::Int, k::Int) = SVector(pos.x[i], pos.y[j], pos.z[k])
-Base.getindex(pos::DoseGrid, i::CartesianIndex{3}) = pos[i[1], i[2], i[3]]
-Base.getindex(pos::DoseGrid, i::Int) = pos[pos.indices[i]]
-
-Base.eachindex(pos::DoseGrid) = eachindex(pos.indices)
-Base.CartesianIndices(pos::DoseGrid) = pos.indices
-
-Base.iterate(pos::DoseGrid) = iterate(pos, 1)
-function Base.iterate(pos::DoseGrid, i)
-    if(i>length(pos)) return nothing end
-    pos[i], i+1
-end
-
-function DoseGrid(Δ::T, bounds) where T<:Number
-    xmin, xmax, ymin, ymax, zmin, zmax = boundsextent(bounds)
-
-    x = xmin:Δ:xmax
-    y = ymin:Δ:ymax
-    z = zmin:Δ:zmax
-
-    grid_index = CartesianIndices( (length(x), length(y), length(z)) )
-    indices = CartesianIndex{3}[]
-    linear_index = zeros(Int, length(x), length(y), length(z))
-
-    # Points
-    for i in grid_index
-        p = SVector(x[i[1]], y[i[2]], z[i[3]])
-        if(within(bounds, p))
-            push!(indices, grid_index[i])
-            linear_index[i] = length(indices)
-        end
-    end
-
-    # Cells
-
-    neighbours = vec([CartesianIndex(i,j,k) for i=0:1, j=0:1, k=0:1])[2:end]
-
-    cells = Vector{Int}[]
-    for i in eachindex(indices)
-        cell = [i]
-        for n in neighbours
-            index = indices[i] + n
-
-            if(checkbounds(Bool, linear_index, index) && linear_index[index] != 0)
-                push!(cell, linear_index[index])
-            end
-        end
-        if(length(cell)==8)
-            push!(cells, cell)
-        end
-    end
-
-    DoseGrid(x, y, z, indices, cells)
-end
-
-
-function vtk_create_cell(cell)
-    n = length(cell)
-    n==5 && return MeshCell(VTKCellTypes.VTK_PYRAMID, cell)
-    n==6 && return MeshCell(VTKCellTypes.VTK_WEDGE, cell)
-    n==8 && return MeshCell(VTKCellTypes.VTK_VOXEL, cell)
-    
-end
-
-function vtk_generate_file(filename, pos::DoseGrid)
-    points = [pos[i] for i in eachindex(pos)]
-    cells = [vtk_create_cell(cell) for cell in pos.cells]
-    vtk_grid(filename, points, cells)
-end
-
-function vtk_grid_data(pos::DoseGrid, arr)  
-    grid_arr = zeros(size(pos))
-    for i in eachindex(arr)
-        grid_arr[pos.indices[i]] = arr[i]
-    end
-    grid_arr
-end
-
-function save(filename::String, pos::DoseGrid, data)
-    vtkfile = vtk_generate_file(filename, pos)
-    for (key, value) in data
-        vtkfile[key, VTKPointData()] = value
-    end
-    vtk_save(vtkfile)
-end
