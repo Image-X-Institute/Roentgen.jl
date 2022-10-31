71d285c9439a5e3a7ca17ec4e7cd5de3fc3f1229
lmejn <33491793+lmejn@users.noreply.github.com>
2022-10-31 14:01:33 +1100

    Added FinitePencilBeamKernel dose calculation alg.
    - Not yet fully implemented
    - Added methods for dose-fluence matrix computations

--- src/DoseCalculationAlgorithms/FinitePencilBeamKernel.jl
+++ src/DoseCalculationAlgorithms/FinitePencilBeamKernel.jl
@@ -33,6 +33,20 @@ function Beamlet(bixel::Bixel, SAD)
     Beamlet(halfwidth, a)
 end
 
+struct FinitePencilBeamKernel{TI1, TI2, T} <: AbstractDoseAlgorithm
+    w::TI1
+    ux::TI2
+    uy::TI2
+    max_radius::T
+end
+
+function FinitePencilBeamKernel(depths, w, ux, uy; max_radius=25.)
+    w = LinearInterpolation(depths, w, extrapolation_bc=Interpolations.Line())
+    ux = LinearInterpolation(depths, ux, extrapolation_bc=Interpolations.Line())
+    uy = LinearInterpolation(depths, uy, extrapolation_bc=Interpolations.Line())
+    FinitePencilBeamKernel(w, ux, uy, max_radius)
+end
+
 #--- Kernel Profile Functions -------------------------------------------------
 
 @inline fpkb_profile_left(x, u, x₀) = sinh(u*x₀)*exp(u*x)
@@ -50,4 +64,68 @@ function fpkb_dose(x, y, w, ux, uy, x₀, y₀)
     fy = fpkb_profile.(y, uy, y₀)
 
     w*fx[1]*fy[1] + (1-w)*fx[2]*fy[2]
+end
+
+#--- Dose-Fluence Matrix Computations -----------------------------------------
+
+"""
+    kernel_size(calc::FinitePencilBeamKernel, pos, beamlets)
+
+Compute the number of bixels with the max. radius of a given position.
+"""
+function kernel_size(calc::FinitePencilBeamKernel, pos::SVector{3, T}, beamlets, SAD::T) where T<:AbstractFloat
+    
+    max_kernel_radius² = calc.max_radius^2
+
+    n = 0
+    for beamlet in beamlets
+        rₐ = dot(pos, beamlet.vector)*beamlet.vector
+        R² = dot(pos - rₐ, pos - rₐ)
+        if(R² < max_kernel_radius²)
+            n += 1
+        end
+    end
+    n
+end
+
+"""
+    point_kernel!(rowval, nzval, pos::AbstractVector{T}, bixels, surf, calc)
+
+Compute the fluence kernel for a given position.
+
+Designed to be used with a dose-fluence matrix of type SparseMatrixCSC. Stores
+the row value in `rowval`, and dose value in `nzval`.
+"""
+function point_kernel!(rowval, nzval, pos::AbstractVector{T}, beamlets, surf, calc::FinitePencilBeamKernel) where T<:AbstractFloat
+
+    SAD = T(1000)
+
+    max_kernel_radius² = calc.max_radius^2
+
+    depth = getdepth(surf, pos)
+
+    depth < zero(T) && return 0
+
+    w = calc.w(depth)
+    ux = calc.ux(depth)
+    uy = calc.uy(depth)
+
+    n = 0
+    for i in eachindex(beamlets)
+        beamlet = beamlets[i]
+
+        Rₐ = dot(pos, beamlet.vector)
+        rₐ = Rₐ*beamlet.vector
+
+        x, y = SAD*(pos[1:2] - rₐ[1:2])/Rₐ
+        x₀, y₀ = beamlet.halfwidth
+
+        R² = dot(pos - rₐ, pos - rₐ)
+        if(R² < max_kernel_radius²)
+            n += 1
+            rowval[n] = i
+            nzval[n] = fpkb_dose(x, y, w, ux, uy, x₀, y₀)/Rₐ^2
+        end
+    end
+    n
 end
