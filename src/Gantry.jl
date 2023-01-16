eceabc2fead15c2ef8d77d92654d48b05a01a3dd
lmejn <33491793+lmejn@users.noreply.github.com>
2023-01-16 11:53:26 +1100

    Modified GantryPosition to store central_beam_axis
    - central_beam_axis is the unit vector pointing from iso to src
    - New method beamaxis returns central_beam_axis
    - getposition still returns position by multiplying central_beam_axis
      by source_axis_distance

--- src/Gantry.jl
+++ src/Gantry.jl
@@ -9,19 +9,19 @@ Stores:
 - `gantry_angle`: As defined by IEC
 - `collimator_angle`: As defined by IEC (beam limiting device angle)
 - `source_axis_distance`: Distance between source and isocenter
-- `position`: Position of the source in the IEC Fixed coordinates
+- `central_beam_axis`: Unit vector pointing from the isocenter to the source in IEC Fixed coordinates.
 """
 struct GantryPosition{T}
     gantry_angle::T
     collimator_angle::T
     source_axis_distance::T
-    position::SVector{3, T}
+    central_beam_axis::SVector{3, T}
 
     function GantryPosition(ϕg, θb, SAD)
         ϕg, θb, SAD = promote(ϕg, θb, SAD)
         T = typeof(ϕg)
-        pos = SAD*SVector(sin(ϕg), zero(T), cos(ϕg))
-        new{T}(ϕg, θb, SAD, pos)
+        ax = SVector(sin(ϕg), zero(T), cos(ϕg))
+        new{T}(ϕg, θb, SAD, ax)
     end
 end
 
@@ -32,8 +32,9 @@ end
 getϕg(gantry::GantryPosition) = gantry.gantry_angle
 getθb(gantry::GantryPosition) = gantry.collimator_angle
 getSAD(gantry::GantryPosition) = gantry.source_axis_distance
+beamaxis(gantry::GantryPosition) = gantry.central_beam_axis
 
-getposition(gantry::GantryPosition) = gantry.position
+getposition(gantry::GantryPosition) = getSAD(gantry)*beamaxis(gantry)
 
 fixed_to_bld(gantry::GantryPosition) = fixed_to_bld(getϕg(gantry), getθb(gantry), getSAD(gantry))
 bld_to_fixed(gantry::GantryPosition) = bld_to_fixed(getϕg(gantry), getθb(gantry), getSAD(gantry))
