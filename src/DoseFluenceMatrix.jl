2205457633d668c542a19748b4dbed00581bfada
33491793+lmejn@users.noreply.github.com <lars.mejnertsen@sydney.edu.au>
2023-02-12 09:01:00 +1100

    Tidying DoseFluenceMatrix.jl

--- src/DoseFluenceMatrix.jl
+++ src/DoseFluenceMatrix.jl
@@ -3,19 +3,13 @@ export dose_fluence_matrix, dose_fluence_matrix!
 #--- Dose-Fluence Matrix-------------------------------------------------------
 
 """
-    dose_fluence_matrix(pos, bixels, bixels, surf, calc)
+    dose_fluence_matrix(pos, beamlets, surf, calc)
 
-Compute a dose-fluence matrix from dose positions, bixels, external surface and
+Compute a dose-fluence matrix from dose positions, beamlets, external surface and
 dose calculation algorithm.
 
 See `dose_fluence_matrix!` for implementation.
 """
-function dose_fluence_matrix(pos, bixels::AbstractVector{<:Bixel}, gantry::GantryPosition,
-                             surf::AbstractExternalSurface, calc::AbstractDoseAlgorithm)
-    D = spzeros(length(pos), length(bixels))
-    dose_fluence_matrix!(D, pos, bixels, gantry, surf, calc)
-end
-
 function dose_fluence_matrix(pos, beamlets::AbstractVector{<:Beamlet},
                              surf::AbstractExternalSurface, calc::AbstractDoseAlgorithm)
     D = spzeros(length(pos), length(beamlets))
@@ -23,39 +17,17 @@ function dose_fluence_matrix(pos, beamlets::AbstractVector{<:Beamlet},
 end
 
 """
-    dose_fluence_matrix!(D, pos, bixels, bixels, surf, calc)
+    dose_fluence_matrix!(D, pos, beamlets, surf, calc)
 
-Compute a dose-fluence matrix from dose positions, bixels, external surface and
+Compute a dose-fluence matrix from dose positions, beamlets, external surface and
 dose calculation algorithm.
 
 Requires the `point_kernel!` method to be defined for the given dose calculation
 algorithm (`calc`). `point_kernel!` computes the dose calculated from the set of
 bixels a given dose point. Stores result in `D`.
 """
-function dose_fluence_matrix!(D::SparseMatrixCSC, pos, bixels::AbstractVector{<:Bixel},
-                              gantry::GantryPosition,
-                              surf::AbstractExternalSurface, calc::AbstractDoseAlgorithm)
-
-    colptr = D.colptr
-    rowval = D.rowval
-    nzval = D.nzval
-
-    # Fill colptr
-    fill_colptr!(D, pos, bixels, gantry, calc)
-
-    # Preallocate arrays
-    nprealloc = colptr[end]-1
-    resize!(nzval, nprealloc)
-    resize!(rowval, nprealloc)
-
-    # Compute row and matrix values
-    dose_kernel!(D, pos, bixels, gantry, surf, calc)
-
-    D
-end
-
 function dose_fluence_matrix!(D::SparseMatrixCSC, pos, beamlets::AbstractVector{<:Beamlet},
-    surf::AbstractExternalSurface, calc::AbstractDoseAlgorithm)
+                              surf::AbstractExternalSurface, calc::AbstractDoseAlgorithm)
 
     colptr = D.colptr
     rowval = D.rowval
