e7653be91940fb4d176db8b66c666255efdc4d89
lmejn <33491793+lmejn@users.noreply.github.com>
2023-05-26 09:41:30 +1000

    Added extra tests for kernel_size

--- test/DoseFluenceMatrix.jl
+++ test/DoseFluenceMatrix.jl
@@ -1,6 +1,33 @@
 
 
 @testset "Kernel Truncation" begin
+
+    # Pre-calculated tests
+    a = SVector(0., 0., 1.)
+    SAD = 2.
+    maxradius = 3.
+    @testset "Pre-calculated Tests" begin
+        r_inside = [SVector(1.,0.,1.), SVector(-2.,0.,3.), SVector(3.,0.,4.)]
+        r_outside = [SVector(4.,0.,2.), SVector(2.,0.,1.), SVector(-6.,0.,3.)]
+        for r in r_inside
+            @test DoseCalculations.kernel_size(r, a, maxradius/SAD)
+        end
+        for r in r_outside
+            @test !DoseCalculations.kernel_size(r, a, maxradius/SAD)
+        end
+
+        for r in r_inside
+            trans = RotYZX(2*π*rand(3)...)
+            @test DoseCalculations.kernel_size(trans*r, trans*a, maxradius/SAD)
+        end
+        for r in r_outside
+            trans = RotYZX(2*π*rand(3)...)
+            @test !DoseCalculations.kernel_size(trans*r, trans*a, maxradius/SAD)
+        end
+    end
+
+    # Arbitrary a
+
     rand_centred(args...) = 2*rand(args...).-1
 
     random_vector() = normalize(SVector(rand_centred(3)...))
