408544ba0f3cf7dcb148498d7091e4ecb980bb73
lmejn <33491793+lmejn@users.noreply.github.com>
2023-06-08 00:32:04 +1000

    Removed test for BixelGrid
    - BixelGrid is no longer in the code

--- test/Bixels.jl
+++ test/Bixels.jl
@@ -68,35 +68,3 @@
         end
     end
 end
-
-@testset "Bixel Grid" begin
-
-    Δ = SVector(1., 2.)
-
-    x = -10.:Δ[1]:10.
-    y = -12.:Δ[2]:10.
-    grid = DoseCalculations.BixelGrid(x, y)
-
-    nx = length(x)-1
-    ny = length(y)-1
-
-    @testset "Grid Size" begin
-        @test size(grid) == (nx, ny)
-        @test length(grid) == nx*ny
-    end
-
-    p = SVector(grid.xc[1], grid.yc[2])
-    w = SVector(grid.Δx[1], grid.Δy[2])
-
-    @testset "Position" begin
-        @test getposition(grid, 1, 2) == p
-        @test getwidth(grid, 1, 2) == w
-    end
-
-    b = Bixel(p, w)
-    @testset "Indexing" begin
-        grid[1, 2] == b # 2D Indexing
-        grid[CartesianIndex(1, 2)] == b # Cartesian Indexing
-        grid[LinearIndices(grid)[1, 2]] == b    # Linear Indexing
-    end
-end
