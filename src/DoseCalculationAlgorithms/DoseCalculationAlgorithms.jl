fbee76a3ac2d657003ddca05f7d9a6e8ade1f2af
lmejn <33491793+lmejn@users.noreply.github.com>
2023-06-08 12:37:05 +1000

    Removed unused methods

--- src/DoseCalculationAlgorithms/DoseCalculationAlgorithms.jl
+++ src/DoseCalculationAlgorithms/DoseCalculationAlgorithms.jl
@@ -3,17 +3,3 @@ abstract type AbstractDoseAlgorithm end
 
 #include("ScaledIsoplaneKernel.jl")
 include("FinitePencilBeamKernel.jl")
-
-"""
-    fluence_grid_length(calc::AbstractDoseAlgorithm)
-
-Return number of bixels in the fluence grid.
-"""
-fluence_grid_length(calc::AbstractDoseAlgorithm) = length(calc.fluence_grid)
-
-"""
-    fluence_grid_size(calc::AbstractDoseAlgorithm)
-
-Return the size of the fluence grid.
-"""
-fluence_grid_size(calc::AbstractDoseAlgorithm) = size(calc.fluence_grid)
