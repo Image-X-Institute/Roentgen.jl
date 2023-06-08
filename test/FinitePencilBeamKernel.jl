588bf5c5b1380da1963b4217ad2c59a2314f9214
lmejn <33491793+lmejn@users.noreply.github.com>
2023-06-08 14:45:58 +1000

    Added tests for FPBK interpolation

--- test/FinitePencilBeamKernel.jl
+++ test/FinitePencilBeamKernel.jl
@@ -30,3 +30,48 @@
     end
 
 end
+
+@testset "Finite Pencil Beam Kernel" begin
+
+    linear(x, a, b) = a*x+b
+    function setup_calc(depthfun, tanθfun)
+        depth = range(0., 300., length=11)
+
+        ux₀ = [0.5, 0.04]
+        uy₀ = [0.4, 0.03]
+
+        w = depthfun.(depth)
+        ux = ux₀'.*depthfun.(depth)
+        uy = uy₀'.*depthfun.(depth)
+
+        parameters = hcat(w, ux, uy)'
+
+        tanθ = range(0., deg2rad(1.), length=9)
+
+        scalingfactor = @. depthfun(depth)*tanθfun(tanθ)'
+
+        depth, tanθ, FinitePencilBeamKernel(parameters, scalingfactor, depth, tanθ)
+    end
+
+    @testset "Parameters and Scaling Factor" begin
+
+        depthfun(x) = linear(x, 0.001, 0.4)
+        tanθfun(x) = linear(x, 0.1, 0.1)
+
+        depth, tanθ, calc = setup_calc(depthfun, tanθfun)
+
+        d = depth[end]*rand()
+        a, ux, uy = DoseCalculations.getparams(calc, d) 
+    
+        @test a ≈ depthfun(d)
+        @test ux ≈ ux₀.*depthfun(d)
+        @test uy ≈ uy₀.*depthfun(d)
+
+        d = depth[end]*rand()
+        t = tanθ[end]*rand()
+
+        A = DoseCalculations.getscalingfactor(calc, d, t)
+        @test A ≈ depthfun(d)*tanθfun(t)
+    end
+
+end
