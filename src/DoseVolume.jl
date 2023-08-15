68f8050773cf80c9c4f70878e5df9d80103f95d1
lmejn <33491793+lmejn@users.noreply.github.com>
2023-08-15 18:01:09 +1000

    Added docstrings to DoseVolume.jl

--- src/DoseVolume.jl
+++ src/DoseVolume.jl
@@ -8,14 +8,35 @@ struct DoseVolume{TPos<:AbstractArray, TSurf<:AbstractExternalSurface} <: Abstra
     surface::TSurf
 end
 
+"""
+    getpositions(vol::DoseVolume)
+
+Get dose positions/grid
+"""
 getpositions(vol::DoseVolume) = vol.positions
+
+"""
+    getsurface(vol::DoseVolume)
+
+Get surface
+"""
 getsurface(vol::DoseVolume) = vol.surface
 
+"""
+    dose_fluence_matrix!(D, vol, beamlets, calc)
+
+Gets positions and surface from `vol`
+"""
 function dose_fluence_matrix!(D, vol::AbstractDoseVolume, beamlets, calc; kwargs...)
     dose_fluence_matrix!(D, getpositions(vol), beamlets, getpositions(surf), calc;
         kwargs...)
 end
 
+"""
+    dose_fluence_matrix(T, vol, beamlets, calc)
+
+Gets positions and surface from `vol`
+"""
 function dose_fluence_matrix(T, vol::AbstractDoseVolume, beamlets, calc; kwargs...)
     dose_fluence_matrix(T, getpositions(vol), beamlets, getpositions(surf), calc;
         kwargs...)
